7d80215da80d5c05825043a8083e9feae97b50ef
Rowdy1002 <rowdy44rowdy@gmail.com>
2024-11-18 07:03:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("fixButton").addEventListener("click", function () {
+    alert("Just kidding! This won't fix it. Enjoy the chaos!");
+    document.body.style.transform = "rotate(0deg)";
+    document.getElementById("content").style.transform = "rotate(0deg)";
+});
